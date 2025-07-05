9c359505c2ecb539e2193c50cb6f126f55c9e075
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-07-05 22:36:17 +0530

    candi modal

--- src/app/candidate-detail-modal/candidate-detail-modal.component.ts
+++ src/app/candidate-detail-modal/candidate-detail-modal.component.ts
@@ -14,10 +14,10 @@
 
 // }
 import { Component, Input, OnInit } from '@angular/core';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { AlertController, IonicModule, ModalController } from '@ionic/angular';
 import { ApiService } from 'src/app/services/api.service';
 // import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+// import 'jspdf-autotable';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -25,19 +25,28 @@ import { CommonModule } from '@angular/common';
   standalone:true,
   imports:[IonicModule,CommonModule],
   templateUrl: './candidate-detail-modal.component.html',
+   styleUrls: ['./candidate-detail-modal.component.scss'],
 })
 export class CandidateDetailModalComponent implements OnInit {
   @Input() userId!: number;
   userData: any;
   loading = true;
-
-  constructor(private modalCtrl: ModalController, private apiService: ApiService) {}
+candidate_id!:number;
+employer_Id!:number;
+// isSameUser:boolean=false;
+pdf:string="";
+  constructor(private modalCtrl: ModalController, private apiService: ApiService,private alertController: AlertController) {}
 
   ngOnInit() {
     this.apiService.candidateDetail({},this.userId).subscribe({
       next: (res) => {
         this.userData = res.data;
         this.loading = false;
+        this.downloadPdf();
+
+        const can_Id=this.userId;
+     console.log( "from can detail",can_Id);
+        // this.isSameUser=true;
       },
       error: () => {
         this.loading = false;
@@ -45,34 +54,54 @@ export class CandidateDetailModalComponent implements OnInit {
         this.dismiss();
       },
     });
+   
   }
 
   dismiss() {
     this.modalCtrl.dismiss();
   }
+  close(){
+     this.modalCtrl.dismiss();
+  }
+  async saveCandi() {
+  // Save logic here (e.g. API call)
+  // Assume candidate is saved successfully
+const storeId=localStorage.getItem('userId');
+this.employer_Id=Number(storeId);
+console.log(" from save ",this.userId);
+ this.apiService.save_candidate(this.employer_Id,this.userId).subscribe({
+    next: (res) => {
+        console.log("saved successfully");
+      },
+      error: () => {
+        console.log("Not saved or already Saved");
+        
+      },
+    });
+  // Show confirmation alert
+  const alert = await this.alertController.create({
+    header: 'Success',
+    message: 'Candidate has been saved successfully!',
+    buttons: ['OK']
+  });
 
-  // downloadPdf() {
-  //   const doc = new jsPDF();
-
-  //   doc.text('Candidate Details', 14, 20);
-
-  //   const rows = [
-  //     ['Name', this.userData.about_me.name ],
-  //     // ['Age', this.userData.age],
-  //     ['Gender', this.userData.about_me.gender],
-  //     // ['Education', this.userData.education],
-  //     // ['Experience', this.userData.experience],
-  //     // ['Skills', this.userData.skills],
-  //     ['Email', this.userData.about_me.email],
-  //     ['Phone', this.userData.about_me.phone],
-  //   ];
-
-  //   (doc as any).autoTable({
-  //     head: [['Field', 'Value']],
-  //     body: rows,
-  //     startY: 30,
-  //   });
-
-  //   doc.save(`${ this.userData.about_me.name}_Details.pdf`);
-  // }
+  await alert.present();
+}
+ 
+  downloadPdf() {
+    this.apiService.downloadResume(this.userId).subscribe(res=>{
+      if (res.status) {
+        this.pdf=res.file_path;
+        
+        //  console.log(this.pdf);
+        console.log("Download Successfully");
+      }
+    });
+    
+  }
+  viewpdf(){
+    if (this.pdf) {
+    window.open(this.pdf, '_blank');
+  }
+  }
 }
