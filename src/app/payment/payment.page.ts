26a5d38d2e4853a105dedbf4b689718d14f9f165
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-09-25 22:30:34 +0530

    placed cards on top

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
   styleUrls: ['./payment.page.scss'],
+  standalone:false,
 })
 export class PaymentPage implements OnInit {
 
