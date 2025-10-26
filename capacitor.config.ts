4a2af778d5f6aff5e377c57395146c710931cdfe
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-10-26 22:47:18 +0530

    minor pro

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ekarigar.kaamchoremp',
   appName: 'kaamChorEmp',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    EdgeToEdge: {
+      backgroundColor: "#511168",
+    },
+  },
 };
 
 export default config;
